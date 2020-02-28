578a099d89deedaea7cc1ff01319de41508888d7
Abdullah Zubair Tanwir <abdullahzubair075@gmail.com>
2020-02-28 21:21:15 +0500

    Initial Commit

---
+++ Capit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Capit
+//
+//  Created by Abdullah on 28/02/2020.
+//  Copyright © 2020 Abdullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
